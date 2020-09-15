a1b84fbfd856cab4da07023ace939da75928ebcb
ayush_98 <ayushagrawal17111998@gmail.com>
2020-09-15 22:16:59 +0530

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const app=express();
+const PORT=process.env.PORT || 8080;
+app.get('/',function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+app.listen(PORT);
